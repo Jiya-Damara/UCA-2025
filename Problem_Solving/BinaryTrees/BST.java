88ef1e09090b96ac90a5693e721549c9ab3153be
Jiya-Damara <damarajiya@gmail.com>
2025-10-10 20:46:01 +0530

    Updated BST.java

--- Problem_Solving/BinaryTrees/BST.java
+++ Problem_Solving/BinaryTrees/BST.java
@@ -14,18 +14,74 @@ public class BST<Key extends Comparable<Key>, Value> {
   }
 
   public Key min() {
+    return min(root);
+  }
+
+  private Key min(Node n) {
+    if (n.left == null) {
+      return n.key;
+    }
+    return min(n.left);
   }
 
   public Key max() {
+    return max(root);
   }
 
+  private Key max(Node n) {
+    if (n.right == null) {
+      return n.key; 
+    }
+    return max(n.right);
+  }
+
+
+  public void delMin() {
+    root = delMin(root);
+  }
+
+  private Node delMin(Node n) {
+    if (n.left == null) {
+      return n.right;
+    }
+    n.left = delMin(n.left);
+  }
+  
   public Key floor(Key key) {
+    return floor(root, key);
+  }
+
+  private Key floor(Node n, Key key) {
+    if (n == null) return null;
+
+    int cmp = key.compareTo(n.key);
+
+    if (cmp == 0) return key;
+    else if (cmp > 0) {
+      key t = floor(n.right, key);
+      if (t == null) return n.key;
+      else return t;
+    }
+    return floor(n.left, key);
   }
 
   public Key ceil(Key key) {
+    return ceil(root, key);
   }
 
-  public Key floor(Key key) {
+  private Key ceil(Node n, Key key) {
+    if(n == null) return null;
+
+    int cmp = key.compareTo(n.key);
+
+    if (cmp == 0) return key;
+    
+  }
+
+  public void del(Key key) {
+  }
+
+  public int rank(Key key) {
   }
   
   public Value get(Key key) {
